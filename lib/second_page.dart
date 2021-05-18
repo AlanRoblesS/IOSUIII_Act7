c8266330f75d14240e5f1dc908b4999e03da90b8
FlutLab.io <>
2021-05-18 14:26:09 +0000

    Initial commit

---
+++ lib/second_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+ 
+class SecondPage extends StatelessWidget {
+  final String texto;
+  TextEditingController _textController = TextEditingController();
+ 
+  SecondPage({Key key, this.texto}) : super(key: key);
+ 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Segunda pantalla Alan Robles"),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(24.0),
+              child: TextField(
+                controller: _textController,
+                maxLength: 10,
+                decoration: InputDecoration(
+                  labelText: "Ingrese nombre",
+                  hintText: "Nombre",
+                ),
+              ),
+            ),
+            MaterialButton(
+              child: Text("Back"),
+              onPressed: () {
+                Navigator.of(context).pop(_textController.text);
+              },
+            ),
+          ],//Fin de widget[]
+        ),//Fin de columna
+      ),//Fin de body center
+    );//Fin de scaffold
+  }//Fin de widget
+}//Fin de secondpage
